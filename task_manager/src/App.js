9eb88b5ec80c18a0bdee0409f1e11ae957aed722
Iniubong Arthur <arthurshur@gmail.com>
2021-08-15 23:08:04 +0100

    task manager react app

--- task_manager/src/App.js
+++ task_manager/src/App.js
@@ -1,12 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Tasks from './Tasks';
+import {useState} from 'react'
+import AddTask from './AddTask';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Study React',
+        day: 'August 14th 7:00pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Study Java',
+        day: 'August 15th 7:00pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Study JavaScript',
+        day: 'August 16th 7:00pm',
+        reminder: false
+    }
+])
+
+//add task 
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 1000) + 1
+
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+//delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter(task => task.id !== id))
+}
+
+//toggle reminder
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map(
+      task => task.id === id ? {...task, reminder 
+        : !task.reminder } : task
+    )
+  )
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
+    <div className="container">
+      <Header title="Task Manager" onAdd={() => setShowAddTask(!showAddTask)} showAdd ={showAddTask}/>
+      {showAddTask && <AddTask addTask ={addTask}/>}
+      {tasks.length > 0 ? 
+      <Tasks tasks={tasks} onDelete={deleteTask} toggle={toggleReminder}/> : 
+      ("No Tasks To Display")}
     </div>
   );
 }
